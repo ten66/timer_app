f1677cc6ca154e6ee8090a4b8baae3c79b468716
ten66 <ikesho.1217@gmail.com>
2023-09-08 22:23:07 +0900

    add: #1 navigation管理用のファイルを作成

---
+++ lib/navigation_manager.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NavigationManager extends StatelessWidget {
+  const NavigationManager({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
