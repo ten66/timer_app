e458a839cd04d1e2eff9c633b6d711c3278dbe43
ten66 <ikesho.1217@gmail.com>
2023-09-11 21:15:17 +0900

    add: #3 timer screenのUI作成

--- lib/screens/timer_screen.dart
+++ lib/screens/timer_screen.dart
@@ -7,7 +7,47 @@ class TimerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
-      child: const Icon(Icons.timer),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            child: const Column(
+              children: [
+                Text(
+                  'title',
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 5),
+          const Text(
+            '00:05:15',
+            style: TextStyle(
+              fontSize: 60,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.stop_outlined),
+                label: const Text('STOP'),
+              ),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(Icons.play_arrow_outlined),
+                label: const Text('START'),
+              ),
+            ],
+          ),
+          const SizedBox(height: 5),
+        ],
+      ),
     );
   }
 }
