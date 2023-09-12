3617caf19ec8a34581f2fa6c411933d64351bb0d
ten66 <ikesho.1217@gmail.com>
2023-09-13 02:12:52 +0900

    add: #5 appbarとお気に入りtext

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,9 +5,42 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      child: const Icon(Icons.home),
+    return Scaffold(
+      appBar: AppBar(
+        // title: const Text('Moti Timer'),
+        title: RichText(
+          text: TextSpan(
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 20,
+            ),
+            children: [
+              TextSpan(
+                text: 'Moti',
+                style: TextStyle(color: Colors.deepPurple[400]),
+              ),
+              const TextSpan(
+                text: 'Time',
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(
+              'お気に入り',
+              style: Theme.of(context)
+                  .textTheme
+                  .headlineSmall!
+                  .copyWith(fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
